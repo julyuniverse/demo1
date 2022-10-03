4d504128ab23933d89add43afd90198da559cb3f
Lee Taesung <julyuniverse@naver.com>
2022-10-03 18:00:12 +0900

    2022-10-03

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Lee Taesung
+ * @since 2022-10-03
+ */
+
+@RestController
+@RequiredArgsConstructor
+public class HelloController {
+    @GetMapping
+    public ResponseEntity<String> hello() {
+        String str = "hello";
+
+        return ResponseEntity.ok().body(str);
+    }
+}
