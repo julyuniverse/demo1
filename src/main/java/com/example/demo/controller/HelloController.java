f60746dcebe402ab55ad67de91089009fbd0c388
Lee Taesung <julyuniverse@naver.com>
2022-10-03 18:14:42 +0900

    2022-10-03

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public ResponseEntity<String> hello() {
-        String str = "hello!";
+        String str1 = "hello!";
 
         return ResponseEntity.ok().body(str);
     }
